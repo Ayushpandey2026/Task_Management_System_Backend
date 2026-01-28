21045be2a5caa36ef0245172a5edfa7f051f27a0
Ayushpandey2026 <ayushpandey02003@gmail.com>
2026-01-28 10:50:41 +0530

    modified the wrong files

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,5 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
-// Connection test karne ke liye (Optional)
-async function testConnection() {
-  try {
-    await prisma.$connect();
-    console.log("✅ Database connected successfully!");
-  } catch (error) {
-    console.error("❌ Database connection failed:", error);
-  }
-}
-
-testConnection();
 
 export default prisma;
